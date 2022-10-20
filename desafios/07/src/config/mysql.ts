13069411a335687a31c2434809d7c2e321472d3b
Luciano Giordano <muecas@gmail.com>
2022-10-20 10:18:18 -0300

    Challenge #07: small fixes

--- desafios/07/src/config/mysql.ts
+++ desafios/07/src/config/mysql.ts
@@ -5,7 +5,7 @@ const options: object = {
 		port     : process.env.DB_PORT     || 3306,
 		user     : process.env.DB_USER     || 'root',
 		password : process.env.DB_PASSWORD || 'muecas78',
-		database : process.env.DB_DB       || 'coderhouse'
+		database : process.env.DB_NAME     || 'coderhouse'
 	}
 }
 
