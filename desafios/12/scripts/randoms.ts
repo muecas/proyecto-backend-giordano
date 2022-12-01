d12dcd1a65b1c0ca44fe247add9856984dd3e712
Luciano Giordano <muecas@gmail.com>
2022-12-01 12:29:50 -0300

    Challenge #12

---
+++ desafios/12/scripts/randoms.ts
@@ -0,0 +1,14 @@
+process.on?.('message', (message: { command: string | undefined, amount: number | undefined }) => {
+	const randoms: Array<number> = [];
+	switch (message?.command) {
+		case 'start':
+			const amount: number = message?.amount || 1e8;
+			for (let x: number = 0, max: number = amount, num: number; x < amount; x = x + 1) {
+				num = Math.ceil(Math.random() * 1000);
+				if (randoms[num]) randoms[num]++;
+				else randoms[num] = 1;
+			}
+			process.send?.({randoms});
+			break;
+	}
+});
